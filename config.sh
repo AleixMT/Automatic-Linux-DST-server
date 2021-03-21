eb7b7a494ed77c1b73f6ad5b23914e1a0fed64f6
serveradmin <serveradmin@DST-server.dst-server-domain>
2021-03-21 02:59:45 +0100

    download steamcmmand line

--- config.sh
+++ config.sh
@@ -1,4 +1,11 @@
-dpkg --add-arquitecture i386
-apt -y update
-apt -y upgrade
-apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 libcurl4-gnutls-dev:i386
+dpkg --add-architecture i386
+apt update -y
+apt upgrade -y
+apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 tmux screen libcurl4-gnutls-dev:i386
+
+
+cd /home/${SUDO_USER} && mkdir -p Steam && cd Steam
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+chown -R ${SUDO_USER} *
+chgrp -R ${SUDO_USER} *
+chmod -R 777 *
