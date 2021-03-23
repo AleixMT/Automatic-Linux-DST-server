bf885755f298be567014ead70e82fc1fce42ac92
dstserver <dstserver@dstserver.dstserver>
2021-03-22 20:58:02 -0500

    added data files

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ apt update -y
 apt upgrade -y
 apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 tmux screen libcurl4-gnutls-dev:i386
 
-
+exit 0
 cd /home/${SUDO_USER} && mkdir -p Steam && cd Steam
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
 chown -R ${SUDO_USER} *
---
+++ modoverrides.lua
@@ -0,0 +1,30 @@
+return {
+--# Camp security 
+["1416161108"] = { enabled = true },
+--# DST Storm Cellar 
+["382177939"] = { enabled = true },
+--# Increased Stack size 
+["374550642"] = { enabled = true },
+--# Show me 
+["666155465"] = { enabled = true },
+--# Simple Health bar
+["1207269058"] = { enabled = true },
+--# Quick drop
+["856487758"] = { enabled = true },
+--# Glowing portal
+["585654889"] = { enabled = true },
+--# Craftable gears
+["817025783"] = { enabled = true },
+--# Only fertilize once 
+["1416161108"] = { enabled = true },
+--# A big bag
+["810443397"] = { enabled = true },
+--# Infinite tent uses
+["356930882"] = { enabled = true },
+--# Tools are fuels
+["599498678"] = { enabled = true },
+--# Large Boats 
+["1839858501"] = { enabled = true },
+--# Moving box 
+["2353205177"] = { enabled = true }
+}
